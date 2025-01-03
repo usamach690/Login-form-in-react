33267fa1432d122d4cf5f81f3651d2f18aab1098
usamach690 <usamatasawar5@gmail.com>
2025-01-03 23:12:26 +0500

    Honda-cars

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,31 +1,155 @@
 import React from "react";
+import Navbar from "../Navbar/Navbar";
 import CarPng from "../../assets/car.png";
+import BuildingImage from "../../assets/building2.jpg";
+import { FaArrowRight } from "react-icons/fa";
+import { motion } from "framer-motion";
+
+const bgImage = {
+  backgroundImage: `url(${BuildingImage})`,
+  backgroundRepeat: "no-repeat",
+  backgroundSize: "cover",
+  backgroundPosition: "center",
+};
 
 const Hero = () => {
   return (
-    <main>
-      <div className="container">
-        {/* Navbar Components here */}
+    <main style={bgImage}>
+      <div
+        className="bg-gradient-to-r from-primary 
+      to bg-primaryDark/90"
+      >
+        <div className="container text-white">
+          {/* Navbar Components here */}
 
-        {/* Hero Section Here */}
+          <Navbar />
 
-        <section className="grid grid-cols-1 md:grid-cols-2 
-        place-items-center min-h-[800px]">
-          {/* text content section */}
-          <div>
+          {/* Hero Section Here */}
 
-          <h1>
-            HONDA <br /> SPORTS EV
-          </h1>
+          <section
+            className="grid grid-cols-1 md:grid-cols-2 
+        place-items-center min-h-[800px]"
+          >
+            {/* text content section */}
+            <div className="space-y-8 mt-[100px]
+            md:mt-[20px] sm:mt-[20px]">
+              <motion.div
+                initial={{ opacity: 0, y: -100 }}
+                whileInView={{ opacity: 1, y: 0 }}
+                transition={{
+                  type: "spring",
+                  stiffness: 100,
+                  damping: 10,
+                  delay: 1,
+                  duration: 0.4,
+                }}
+                className="space-y-8"
+              >
+                <h1
+                  className="text-5xl lg:text-7xl 
+              font-bold leading-tight uppercase"
+                >
+                  Honda <br />{" "}
+                  <span
+                    className="text-transparent 
+                  text-outline"
+                  >
+                    Sports EV
+                  </span>
+                </h1>
+                <button
+                  className="outline-btn flex 
+              justify-center items-center gap-4 group"
+                >
+                  Explore
+                  <FaArrowRight
+                    className="group-hover:translate-x-2
+                transition"
+                  />
+                </button>
+              </motion.div>
 
-          </div>
-          <button>Explore</button>
+              {/* features section here */}
+              <motion.div
+                initial={{ opacity: 0, y: 100 }}
+                whileInView={{ opacity: 1, y: 0 }}
+                transition={{
+                  type: "spring",
+                  stiffness: 100,
+                  damping: 10,
+                  delay: 1.2,
+                  duration: 0.4,
+                }}
+                className="bg-black/40 p-6 space-y-2
+              lg:max-w-[350px] !mt-[60px]"
+              >
+                <h1 className="text-xl uppercase">Vehicle Features</h1>
+                <p className="text-xs leading-loose">
+                  The car features rear-hinged suicide doors. Yuki Terai is
+                  credited with the exterior design. The interior fits four
+                  adults, with the front passengers seated on a single bench
+                  seat. The dashboard is decorated in wood trim with a long
+                  digital screen as the instrument and infotainment panel
+                </p>
+              </motion.div>
+            </div>
 
-          {/* image section */}
-          <div>
-            <img src={CarPng} alt="" />
-          </div>
-        </section>
+            {/* image section */}
+            <div className="relative">
+              <motion.img
+                initial={{ opacity: 0, x: 200 }}
+                whileInView={{ opacity: 1, x: 0, scale: 1.4 }}
+                transition={{
+                  duration: 1,
+                }}
+                src={CarPng}
+                alt=""
+                className="w-full relative z-30"
+              />
+
+              {/* Glowing Circle */}
+              <motion.div
+              initial={{ opacity: 0, y: 0, scale: 0.5 }}
+              whileInView={{opacity: 1, y: 0, scale:1}}
+              transition={{
+                type: "spring",
+                stiffness: 100,
+                damping: 10,
+                delay: 0.8,
+                duration: 0.8,
+              }}
+                className="border-[20px]
+              border-cyan-200 rounded-full
+              h-[180px] w-[180px] md:h-[220px] 
+              md:w-[220px] absolute left-1/2
+              top-0 glowing-shadow"
+              ></motion.div>
+
+              {/* Bg Text here */}
+              <motion.div
+              initial={{ opacity: 0, x: -100 }}
+              whileInView={{opacity: 1, x: 0}}
+              transition={{
+                type: "spring",
+                stiffness: 100,
+                damping: 10,
+                delay: 1.2,
+                duration: 0.8,
+              }}
+                className="absolute -top-40
+              left-0 z-[1]"
+              >
+                <h1
+                  className="text-[180px]
+                font-bold text-cyan-200/10
+                leading-none"
+                >
+                  Sport
+                </h1>
+              </motion.div>
+            </div>
+          </section>
+        </div>
       </div>
     </main>
   );
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,8 +1,82 @@
-import React from 'react'
+import React from "react";
+import Logo from "../../assets/honda-logo.png";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { FaSearch } from "react-icons/fa";
+import { motion } from "framer-motion";
+
+const NavMenu = [
+  {
+    id: 1,
+    title: "Home",
+    link: "#",
+  },
+  {
+    id: 2,
+    title: "About us",
+    link: "#",
+  },
+  {
+    id: 3,
+    title: "Bikes",
+    link: "#",
+  },
+  {
+    id: 4,
+    title: "Cars",
+    link: "#",
+  },
+];
 
 const Navbar = () => {
-  return <div>Navbar</div>
-  
-}
+  return (
+    <main
+      className="py-10 top-0 left-0 w-ful
+    pt-10 z-20 text-white "
+    >
+      <motion.div
+        initial={{ opacity: 0 }}
+        whileInView={{ opacity: 1, }}
+        className="container"
+      >
+        <div
+          className="flex
+      justify-between items-center"
+        >
+          {/* Logo section */}
+          <div>
+            <img src={Logo} alt="" className="w-[80px] invert" />
+          </div>
+          {/* Nav Menu Section */}
+          <div className="hidden md:block">
+            <ul className="flex gap-4">
+              {" "}
+              {NavMenu.map((menu) => (
+                <li key={menu.id}>
+                  <a
+                    href={menu.link}
+                    className="inline-block px-6 py-2
+                uppercase hover:bg-primary duration-200 rounded-md
+                hover:shadow-[0px_0px_20px_8px_#d2e6ff]"
+                  >
+                    {menu.title}
+                  </a>
+                </li>
+              ))}
+            </ul>
+          </div>
+          {/* Hamburger Menu Section */}
+          <div className="flex items-center gap-6">
+            <div className="rounded-icons">
+              <GiHamburgerMenu className="text-xl" />
+            </div>
+            <div className="rounded-icons">
+              <FaSearch className="text-xl" />
+            </div>
+          </div>
+        </div>
+      </motion.div>
+    </main>
+  );
+};
 
-export default Navbar
+export default Navbar;
